7ff09022e195f98b03857d61e4c86911712b95ab
random197854 <delsin.rowe91@gmail.com>
2026-02-28 03:15:56 +0000

    add assets 28-02-2026 03:15:55

--- data/scripts/data/meta.js
+++ data/scripts/data/meta.js
@@ -317,13 +317,24 @@ var ARTIST = {
         jap: "絵かきの甲殻類",
         japAlias: []
     },
+    SANAI_AKIRA:{
+        eng: "Sanai Akira",
+        engAlias: ["Sanai", "Akira Sanai", "Akira"],
+        jap: "才乃麗",
+        japAlias: ["才乃", "麗"]
+    },
+    INGFOX:{
+        eng: "Ingfox",
+        engAlias: [],
+        jap: "Ingfox",
+        japAlias: []
+    },
     IGNORE:{
         eng: "",
         engAlias: [],
         jap: "",
         japAlias: []
     }
-    
 };
 
 var CV = {
@@ -1394,6 +1405,18 @@ var CV = {
         engAlias: ["Sakurano", "Kohane Sakurano", "Kohane"],
         jap: "桜野恋羽",
         japAlias: ["桜野", "恋羽"]
+    },
+    YUZU_HACHIMITSU:{
+        eng: "Yuzu Hachimitsu",
+        engAlias: ["Yuzu", "Hachimitsu Yuzu", "Hachimitsu"],
+        jap: "ゆずはちみつ",
+        japAlias: ["ゆず", "はちみつ"]
+    },
+    KAGURAZAKA_KIYO:{
+        eng: "Kagurazaka Kiyo",
+        engAlias: ["Kagurazaka", "Kiyo Kagurazaka", "Kiyo"],
+        jap: "神楽坂きよ",
+        japAlias: ["神楽坂", "きよ"]
     },
     IGNORE:{
         eng: "",
@@ -11880,7 +11903,7 @@ var CHAR = {
                 TAG.TANNED_SKIN, TAG.BIG_BREASTS
             ],
             gender:"female",
-            originalCharacter:true,
+            originalCharacter:false,
             artist:ARTIST.KAGAMI_HIROTAKA,
             cv:CV.GOMANJIRU
         }
@@ -11902,6 +11925,82 @@ var CHAR = {
             originalCharacter:true,
             artist:ARTIST.YOU_MARU,
             cv:CV.SAKURANO_KOHANE
+        }
+    },
+    CHIKUBUSHIMA_ISANA:{
+        base:{
+            name:{
+                eng:"Chikubushima Isana",
+                engAlias:["Chikubushima", "Isana Chikubushima", "Isana"],
+                jap:"竹生島イサナ",
+                japAlias:["竹生島", "イサナ"]
+            },
+            tags:[
+                TAG.CYAN_HAIR, TAG.LONG_HAIR,
+                TAG.UNNATURAL_SCLERA, TAG.CYAN_EYES,
+                TAG.UNNATURAL_SKIN, TAG.HUGE_BREASTS
+            ],
+            gender:"female",
+            originalCharacter:true,
+            artist:ARTIST.SANAI_AKIRA,
+            cv:CV.OUKAWA_MIO
+        }
+    },
+    SUMINOE_MADARA:{
+        base:{
+            name:{
+                eng:"Suminoe Madara",
+                engAlias:["Suminoe", "Madara Suminoe", "Madara"],
+                jap:"墨之江まだら",
+                japAlias:["墨之江", "まだら"]
+            },
+            tags:[
+                TAG.BLOND_HAIR, TAG.BROWN_HAIR, TAG.MULTICOLORED_HAIR, TAG.SHORT_HAIR,
+                TAG.VIOLET_EYES,
+                TAG.LIGHT_SKIN, TAG.SMALL_BREASTS
+            ],
+            gender:"female",
+            originalCharacter:true,
+            artist:ARTIST.JUMBO_YAMA,
+            cv:CV.MOMOZONO_ANRI
+        }
+    },
+    MOMOCHI_NAGI:{
+        base:{
+            name:{
+                eng:"Momochi Nagi",
+                engAlias:["Momochi", "Nagi Momochi", "Nagi"],
+                jap:"桃地凪",
+                japAlias:["桃地", "凪"]
+            },
+            tags:[
+                TAG.SHOULDER_LENGTH_HAIR, TAG.BLACK_HAIR,
+                TAG.BLUE_EYES,
+                TAG.BIG_BREASTS, TAG.LIGHT_SKIN
+            ],
+            gender:"female",
+            originalCharacter:true,
+            artist:ARTIST.INGFOX,
+            cv:CV.YUZU_HACHIMITSU
+        }
+    },
+    KURAMOCHI_KEITO:{
+        base:{
+            name:{
+                eng:"Kuramochi Keito",
+                engAlias:["Kuramochi", "Keito Kuramochi", "Keito"],
+                jap:"倉持恵都",
+                japAlias:["倉持", "恵都"]
+            },
+            tags:[
+                TAG.BLUE_HAIR, TAG.SHOULDER_LENGTH_HAIR,
+                TAG.BLUE_EYES,
+                TAG.AVERAGE_BREASTS, TAG.LIGHT_SKIN
+            ],
+            gender:"female",
+            originalCharacter:true,
+            artist:ARTIST.HAMADA_YOUHO,
+            cv:CV.KAGURAZAKA_KIYO
         }
     },
 
@@ -33756,6 +33855,89 @@ var SCENE = {
         },
         ignoredCharacterTags:[]
     },
+    "1021_1":{
+        character:CHAR.MARDOLL,
+        tags:{
+            female:[
+                TAG.THIGH_HIGH_STOCKINGS, TAG.BREASTS_OUT,
+                TAG.FUTANARI, TAG.SEX_ENGINE, TAG.AHEGAO
+            ],
+            male:[],
+            location:[TAG.BROTHEL, TAG.PRIVATE],
+            misc:[TAG.NO_SEX]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"1021_1"
+    },
+    "1021_2":{
+        character:CHAR.MARDOLL,
+        tags:{
+            female:[
+                TAG.THIGH_HIGH_STOCKINGS, TAG.BREASTS_OUT,
+                TAG.FUTANARI, TAG.BLOWJOB, TAG.BUKKAKE, TAG.CUM_IN_MOUTH, TAG.VAGINAL_SEX, TAG.NAKADASHI
+            ],
+            male:[],
+            location:[TAG.BROTHEL, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[]
+    },
+    "1022_1":{
+        character:[CHAR.KAMIMURA_MAIKA, CHAR.FUUMA_KOTAROU],
+        form:["young", null],
+        tags:{
+            female:[
+                TAG.TAIMANIN_OUTFIT, TAG.BREASTS_OUT, TAG.NIPPLE_PIERCING, TAG.PANTYLESS, TAG.THIGH_HIGH_STOCKINGS, TAG.BODY_WRITING,
+                TAG.VAGINAL_FINGERING, TAG.BLOWJOB, TAG.SQUIRTING, TAG.AHEGAO, TAG.PAIZURI, TAG.BUKKAKE
+            ],
+            male:[],
+            location:[TAG.LOVE_HOTEL, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"1022_2"
+    },
+    "1022_2":{
+        character:[CHAR.KAMIMURA_MAIKA, CHAR.FUUMA_KOTAROU],
+        form:["young", null],
+        tags:{
+            female:[
+                TAG.TAIMANIN_OUTFIT, TAG.BREASTS_OUT, TAG.NIPPLE_PIERCING, TAG.PANTYLESS, TAG.BODY_WRITING,
+                TAG.VAGINAL_SEX, TAG.BREAST_GROPING, TAG.NAKADASHI, TAG.AHEGAO, TAG.NIPPLE_PINCHING
+            ],
+            male:[],
+            location:[TAG.LOVE_HOTEL, TAG.PRIVATE],
+            misc:[TAG.CUT_IN_XRAY]
+        },
+        ignoredCharacterTags:[]
+    },
+    "1023":{
+        character:CHAR.CHIKUBUSHIMA_ISANA,
+        tags:{
+            female:[
+                TAG.TAIMANIN_OUTFIT, TAG.BREASTS_OUT, TAG.PANTIES_PUSHED_TO_SIDE,
+                TAG.VAGINAL_LEAKAGE, TAG.SQUIRTING, TAG.VAGINAL_SEX, TAG.NAKADASHI
+            ],
+            male:[],
+            location:[TAG.LOVE_HOTEL, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[]
+    },
+    "1024":{
+        character:CHAR.SUMINOE_MADARA,
+        tags:{
+            female:[
+                TAG.TAIMANIN_OUTFIT, TAG.RIPPED_CLOTHING, TAG.BREAST_CUTOUTS, TAG.VAGINAL_CUTOUT, TAG.WOMB_TATTOO,
+                TAG.STANDING_SEX,
+                TAG.SQUIRTING, TAG.VAGINAL_SEX, TAG.NAKADASHI, TAG.AHEGAO
+            ],
+            male:[],
+            location:[TAG.PRISON_CELL, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[]
+    },
 
 
     
@@ -33996,6 +34178,88 @@ var SCENE = {
             misc:[]
         },
         ignoredCharacterTags:[]
+    },
+    "4009_1":{
+        character:CHAR.MOMOCHI_NAGI,
+        tags:{
+            female:[
+                TAG.BRA, TAG.PANTIES, TAG.BREASTS_OUT,
+                TAG.ROPE_BONDAGE, TAG.ROTOR, TAG.DRUGS, TAG.AHEGAO, TAG.VAGINAL_LEAKAGE, TAG.VAGINAL_SEX, TAG.BREAST_GROPING, TAG.NAKADASHI, TAG.AHEGAO, TAG.BUKKAKE
+            ],
+            male:[],
+            location:[TAG.BEDROOM, TAG.ON_BED, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"4009_2"
+    },
+    "4009_2":{
+        character:CHAR.MOMOCHI_NAGI,
+        tags:{
+            female:[
+                TAG.KIMONO, TAG.BREASTS_OUT, TAG.PANTYLESS, TAG.HALF_NAKED, TAG.NAKED,
+                TAG.HANDJOB, TAG.VAGINAL_SEX, TAG.NAKADASHI
+            ],
+            male:[],
+            location:[TAG.INSIDE, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[]
+    },
+    "4009_3":{
+        character:CHAR.MOMOCHI_NAGI,
+        tags:{
+            female:[
+                TAG.BRA, TAG.PANTIES, TAG.BREASTS_OUT,
+                TAG.ROPE_BONDAGE, TAG.ROTOR, TAG.DRUGS, TAG.AHEGAO, TAG.VAGINAL_LEAKAGE, TAG.VAGINAL_SEX, TAG.BREAST_GROPING, TAG.NAKADASHI, TAG.AHEGAO, TAG.BUKKAKE
+            ],
+            male:[],
+            location:[TAG.BEDROOM, TAG.ON_BED, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"4009_2"
+    },
+    "4010_1":{
+        character:CHAR.KURAMOCHI_KEITO,
+        tags:{
+            female:[
+                TAG.NAKED, TAG.NAVEL_PIERCING, TAG.NIPPLE_PIERCING, TAG.GENITAL_PIERCING,
+                TAG.WOMB_TATTOO, TAG.BLOWJOB, TAG.MASTURBATION, TAG.BREAST_GROPING, TAG.CUM_IN_MOUTH, TAG.VAGINAL_SEX, TAG.BUKKAKE, TAG.AHEGAO
+            ],
+            male:[TAG.ANILINGUS],
+            location:[],
+            misc:[]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"4010_2"
+    },
+    "4010_2":{
+        character:CHAR.KURAMOCHI_KEITO,
+        tags:{
+            female:[
+                TAG.TAIMANIN_OUTFIT, TAG.BREAST_CUTOUTS, TAG.VAGINAL_CUTOUT,
+                TAG.BREAST_GROPING, TAG.ANAL_FINGERING, TAG.ANAL_SEX, TAG.ASPHYXIATION, TAG.STOMACH_DEFORMATION, TAG.NAKADASHI, TAG.AHEGAO
+            ],
+            male:[],
+            location:[TAG.OUTSIDE, TAG.PRIVATE],
+            misc:[]
+        },
+        ignoredCharacterTags:[]
+    },
+    "4010_3":{
+        character:CHAR.KURAMOCHI_KEITO,
+        tags:{
+            female:[
+                TAG.NAKED, TAG.NAVEL_PIERCING, TAG.NIPPLE_PIERCING, TAG.GENITAL_PIERCING,
+                TAG.WOMB_TATTOO, TAG.BLOWJOB, TAG.MASTURBATION, TAG.BREAST_GROPING, TAG.CUM_IN_MOUTH, TAG.VAGINAL_SEX, TAG.BUKKAKE, TAG.AHEGAO
+            ],
+            male:[TAG.ANILINGUS],
+            location:[],
+            misc:[]
+        },
+        ignoredCharacterTags:[],
+        nextScene:"4010_2"
     },
     "5003_1":{
         character:CHAR.KOKUOUIN_YORUYA,
